46a067be3d022a8986d255f9c709e00c39bf8774
hsun711 <hsun711@gmail.com>
2021-08-24 13:54:16 +0800

    use map to loop the section

--- react_page/src/NextSection.js
+++ react_page/src/NextSection.js
@@ -1,48 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import caveman from './img/caveman-small.jpg';
 import cook from './img/cook-small.jpg';
 import doctor from './img/doctor-small.jpg';
 import engineer from './img/engineer-small.jpg';
 
-const NextSection = () => {
-    return (
-        <div>
-            <section className="container ">
-                <div className="character">
-                    <img src={caveman} alt="caveman" />
-                    <div className="pro">
-                        <h4>Viking</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.</p>
-                    </div>
-                </div>
+const characters = [
+    {
+        img: caveman,
+        name: 'Caveman',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla.'
+    },
+    {
+        img: cook,
+        name: 'Cook',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis.'
+    },
+    {
+        img: doctor,
+        name: 'Doctor',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.'
+    },
+    {
+        img: engineer,
+        name: 'Engineer',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.'
+    }
 
-                <div className="character">
-                    <img src={cook} alt="cook" />
-                    <div className="pro">
-                        <h4>Zombie</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.</p>
-                    </div>
-                </div>
-            </section>
-            <section className="container">
-                <div className="character">
-                    <img src={doctor} alt="doctor" />
-                    <div className="pro">
-                        <h4>Viking</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.</p>
-                    </div>
-                </div>
+];
 
-                <div className="character">
-                    <img src={engineer} alt="engineer" />
-                    <div className="pro">
-                        <h4>Zombie</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.</p>
-                    </div>
-                </div>
-            </section>
-        </div>
-    )
+class NextSection extends Component {
+    render() {
+        return(
+            <div>
+                <section className="container">
+                    {characters.map( char => (
+                        <div className="character">
+                            <img src={char.img} alt="pirate" />
+                            <div className="pro">
+                                <h4>{char.name}</h4>
+                                <p>{char.desc}</p>
+                            </div>
+                        </div>
+                    ))}
+                </section>
+            </div>
+        );
+    }
 }
 
 export default NextSection
--- react_page/src/Section.js
+++ react_page/src/Section.js
@@ -1,50 +1,95 @@
-import React from 'react';
+import React, {Component} from 'react';
 import pirate from './img/pirate.jpg';
 import vendor from './img/vendor.jpg';
 import viking from './img/viking.jpg';
 import zombie from './img/zombie.jpg';
 
-const Section = () => {
-    return (
-        <div>
-            <section className="container">
-                <div className="character">
-                    <img src={pirate} alt="pirate" />
-                    <div className="pro">
-                        <h4>Pirate</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla.</p>
-                    </div>
-                </div>
+const characters = [
+    {
+        img: pirate,
+        name: 'Pirate',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla.'
+    },
+    {
+        img: vendor,
+        name: 'Vendor',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis.'
+    },
+    {
+        img: viking,
+        name: 'Viking',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.'
+    },
+    {
+        img: zombie,
+        name: 'Zombie',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.'
+    }
 
-                <div className="character">
-                    <img src={vendor} alt="vendor" />
-                    <div className="pro">
-                        <h4>Vendor</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis.</p>
-                    </div>
-                </div>
-            </section>
-            <section className="container">
-                <div className="character">
-                    <img src={viking} alt="viking" />
-                    <div className="pro">
-                        <h4>Viking</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.</p>
-                    </div>
-                </div>
+];
 
-                <div className="character">
-                    <img src={zombie} alt="zombie" />
-                    <div className="pro">
-                        <h4>Zombie</h4>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.</p>
-                    </div>
-                </div>
-            </section>
-        </div>
-            
-    )
+class Section extends Component {
+    render() {
+        return(
+            <div>
+                <section className="container">
+                    {characters.map( char => (
+                        <div className="character">
+                            <img src={char.img} alt="pirate" />
+                            <div className="pro">
+                                <h4>{char.name}</h4>
+                                <p>{char.desc}</p>
+                            </div>
+                        </div>
+                    ))}
+                </section>
+            </div>
+        );
+    }
 }
+
+
+//  const Section = () => {
+//      return (
+//          <div>
+//              <section className="container">
+//                  <div className="character">
+//                      <img src={pirate} alt="pirate" />
+//                      <div className="pro">
+//                          <h4>Pirate</h4>
+//                          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla.</p>
+//                     </div>
+//                  </div>
+
+//                  <div className="character">
+//                      <img src={vendor} alt="vendor" />
+//                      <div className="pro">
+//                          <h4>Vendor</h4>
+//                          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis.</p>
+//                      </div>
+//                  </div>
+//              </section>
+//              <section className="container">
+//                  <div className="character">
+//                      <img src={viking} alt="viking" />
+//                      <div className="pro">
+//                          <h4>Viking</h4>
+//                          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque.</p>
+//                      </div>
+//                  </div>
+
+//                  <div className="character">
+//                      <img src={zombie} alt="zombie" />
+//                      <div className="pro">
+//                          <h4>Zombie</h4>
+//                          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ab quae cupiditate mollitia eaque modi impedit unde facilis veritatis reiciendis nulla voluptatum excepturi esse.</p>
+//                      </div>
+//                  </div>
+//              </section>
+//          </div>
+            
+//      )
+//  }
 
 export default Section
 
